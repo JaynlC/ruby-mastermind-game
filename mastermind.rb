56cd9a04023b30999693be05b0302be54688487c
Jay C <jc30g14@soton.ac.uk>
2022-10-11 00:28:46 +0100

    puts messages in player_guess method, in Game class cut to Messages module. Further player input checks added to player guess method.

--- mastermind.rb
+++ mastermind.rb
@@ -6,6 +6,14 @@ module Messages
   def greeting(player)
     puts "Welcome #{player.name} to Mastermind!"
   end
+
+  def player_choose_color
+    "Guess (and spell correctly) the four colors in the correct order. Type your color choice, then press enter. Repeat till four colors are selected:"
+  end
+
+  def player_input_incorrect(colors)
+    "Please pick and spell correctly from the following six colors: #{colors}"
+  end
 end
 
 module BoardColors
@@ -45,7 +53,7 @@ end
 class Game
   include Messages, BoardColors
 
-  attr_reader :code_to_crack
+  attr_reader :code_to_crack, :player_all_guesses
 
   def initialize()
     @code_to_crack = Computer.new.computer_code # This is how you call something from new class into an existing class. Create an instance of it. 
@@ -53,35 +61,42 @@ class Game
   
   def greeting(player)
     super
-    puts "This is all colors: #{board_colors()}"
+    puts "This is the six colors you can choose from: #{board_colors()}"
     player_guess()
   end
 
   private
 
   def player_guess
-    puts "This is code to crack: #{code_to_crack}" #for troubleshooting, will be deleted in final draft
+    puts "This is code to crack: #{code_to_crack}" # troubleshooting, will be deleted in final draft
     colors = board_colors()
     @player_all_guesses = []
-
+    puts player_choose_color()
     4.times do
       @player_spelt_colors = false
       until @player_spelt_colors
-        puts "Guess (and spell correctly) the four colors in the correct order. Type your color choice, then press enter. Repeat till four colors are selected:"
         @player_color_choice = gets.chomp.downcase
         for j in (0...colors.length) 
           if @player_color_choice == colors[j]
-            @player_all_guesses.push(@player_color_choice)
-            puts "Your guesses are: #{@player_all_guesses}"
+            player_all_guesses.push(@player_color_choice)
+            puts "Your guesses are: #{player_all_guesses}"
             @player_spelt_colors = true
             break
           end
         end
+        unless @player_spelt_colors then puts player_input_incorrect(colors)
+        end
       end
     end
-    puts "Your four color guesses are: #{@player_all_guesses}"
+    compare_choice()
+  end
+
+  def compare_choice
+    puts "Your four color guesses are: #{player_all_guesses}"
+    #resume here
   end
 end
+
 
 def start_game
   player = Player.new
