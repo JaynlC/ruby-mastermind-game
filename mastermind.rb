53ac049be459b75473efb2e977dc7be552e59b2c
Jay C <jc30g14@soton.ac.uk>
2022-10-14 01:22:50 +0100

    refactored code by renaming methods. Matches and partial matches now working as intended. Player can now play as code cracker.

--- mastermind.rb
+++ mastermind.rb
@@ -56,7 +56,7 @@ class Game
   attr_reader :code_to_crack, :player_all_guesses
 
   def initialize()
-    @code_to_crack = Computer.new.computer_code # This is how you call something from new class into an existing class. Create an instance of it. 
+    @code_to_crack = Computer.new.computer_code # This is how you call something from new class into an existing class. Create an instance of it.
   end
   
   def greeting(player)
@@ -68,7 +68,7 @@ class Game
   private
 
   def player_guess
-    puts "This is code to crack: #{code_to_crack}" # troubleshooting, will be deleted in final draft
+    puts "This is the code to crack in player_guess #{code_to_crack}"
     colors = board_colors()
     @player_all_guesses = []
     puts player_choose_color()
@@ -89,30 +89,53 @@ class Game
       end
     end
     puts "Your four color guesses are: #{player_all_guesses}"
-    compare_choice()
+    feedback_variables(player_all_guesses, code_to_crack)
   end
 
-  def compare_choice
+  def feedback_variables(player_code, computer_code)
+    puts "This is code to crack: #{computer_code}" # troubleshooting, will be deleted in final draft
     @@matches = 0
     @@partials = 0
-    @code_to_crack_analyse = code_to_crack
-    @player_all_guesses_analyse = player_all_guesses
-    @player_all_guesses_analyse.each_with_index do |player_color, player_color_index| 
-      @code_to_crack_analyse.each_with_index do |computer_color, computer_color_index|
-        if player_color == computer_color && player_color_index == computer_color_index
-          @@matches += 1
-          puts "match for #{player_color}"
-          @code_to_crack_analyse.delete_at(computer_color_index)
-          @player_all_guesses_analyse.delete_at(player_color_index)
-          # might need to use i and j loops instead. 
-        elsif player_color == computer_color && player_color_index != computer_color_index
+    analyse_matches(player_code.dup, computer_code.dup)
+  end
+
+  def delete_match_indices(color_code, match_indices)
+    color_code.reject!.with_index {|_color, index| match_indices.include? index }
+  end
+
+  def analyse_matches(player_code, computer_code)
+    @match_color_index = []
+    player_code.each_with_index do |player_color, player_color_index| 
+      if player_color == computer_code[player_color_index]
+        @@matches += 1
+        @match_color_index.push(player_color_index)
+        puts "match for #{player_color}"
+        puts "this is code to crack (matches): #{computer_code}"
+        puts "this is playercode (matches): #{player_code}"
+      end
+    end
+    puts @match_color_index
+    delete_match_indices(player_code, @match_color_index)
+    delete_match_indices(computer_code, @match_color_index)
+    #delete below puts after testing code. 
+    puts "This is player code pst matches: #{player_code}"
+    puts "This is computer code pst matches: #{computer_code}"
+    analyse_partials(player_code.dup, computer_code.dup)
+  end
+
+  def analyse_partials(player_code, computer_code)
+    player_code.each_with_index do |player_color, player_color_index| 
+      computer_code.each_with_index do |computer_color, computer_color_index|
+        if player_color == computer_color && player_color_index != computer_color_index
           @@partials += 1
-          # @@partials -= @@matches
           puts "partial for #{player_color}"
+          computer_code.delete_at(computer_color_index)
+          puts "this is code to crack (partials]): #{computer_code}"
+          puts "this is playercode (partials): #{player_code}"
+          break
         end
       end
     end
-    
     puts "Total Matches: #{@@matches}"
     puts "Total Partials: #{@@partials}"
     @@matches == 4 ? winner() : player_guess()
